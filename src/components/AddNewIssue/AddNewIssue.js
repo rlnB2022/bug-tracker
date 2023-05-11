83316c5b464fbb944e616a8441f4d461e7bcdcc9
rlnB2022 <beyer.richard73@gmail.com>
2023-05-10 22:59:42 -0500

    Added padding to MobileNavigation. Included uuid for creating unique ids

--- src/components/AddNewIssue/AddNewIssue.js
+++ src/components/AddNewIssue/AddNewIssue.js
@@ -1,8 +1,10 @@
 import './AddNewIssue.css';
 import { useSelector } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
 
 const AddNewIssue = () => {
     const visible = useSelector(state => state.addNewIssueVisible);
+    const id = uuidv4();
 
     return (
         <div className={`add-new-issue-container ${visible ? 'add-new-issue-visible' : ''}`}>
