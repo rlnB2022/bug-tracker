37e7c919c9a3c5a2876e72a2428620a54a6e2b62
rlnB2022 <beyer.richard73@gmail.com>
2023-05-11 12:41:36 -0500

    Notifications panel working. Can choose to Mark as Read for notifications and navigation button at bottom will update as well.

--- src/components/NotificationsCount/NotificationsCount.js
+++ src/components/NotificationsCount/NotificationsCount.js
@@ -3,10 +3,10 @@ import { useSelector } from 'react-redux';
 
 const NotificationsCount = () => {
     /* Get the number of notifications, if any */
-    const count = useSelector(state => state.notifications).length;
+    const unreadCount = useSelector(state => state.notifications.filter(obj => obj.read === false));
 
     return (
-        <div className="count-container">{count}</div>
+        <div className={`count-container ${unreadCount.length > 0 ? 'visible' : ''}`}>{unreadCount.length > 0 && unreadCount.length}</div>
     )
 };
 
